475977bc0e803fbedf2bb9fb1f0745e9ea970769
sabrinaquiroga <sabriquiroga@yahoo.es>
2018-05-16 19:47:25 -0400

    creacion de personas

---
+++ src/main/java/Persona2.java
@@ -0,0 +1,5 @@
+/**
+ * Created by Ing. Rafael Serate on 15/05/18.
+ */
+public class Persona2 extends Personas {
+}
---
+++ src/main/java/Personas.java
@@ -0,0 +1,42 @@
+
+public class Personas {
+    String nombre;
+    String apellido;
+    int edad;
+    String direccion;
+
+    public void setNombre (String nombre){
+        this.nombre=nombre;
+
+    }
+    public String getNombre(){
+        return nombre;
+
+    }
+    public void setApellido (String apellido){
+        this.apellido=apellido;
+
+    }
+    public String getApellido(){
+        return apellido;
+
+    }
+    public void setEdad (int edad){
+        this.edad=edad;
+
+    }
+    public int getEdad(){
+        return edad;
+
+    }
+    public void setDireccion (String direccion){
+        this.direccion=direccion;
+
+    }
+    public String getDireccion(){
+        return direccion;
+
+    }
+
+}
+
